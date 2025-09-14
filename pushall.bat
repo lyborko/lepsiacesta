0b13430231568dfc2e26044e6d0354bf095c87cb
lyborko <lyborko@azet.sk>
2025-09-14 12:00:29 +0200

    one

--- pushall.bat
+++ pushall.bat
@@ -1,10 +1,19 @@
-@REM call drush sql:dump --result-file=C:\xampp\htdocs\lepsiacesta\db_lepsiacesta.sql
-@REM mysqldump -u root lepsiacesta > db_lepsiacesta.sql
+@REM Dump databázy
 mysqldump --host="localhost" --user="root" --password="" --databases lepsiacesta > "C:/xampp/htdocs/lepsiacesta/db_lepsiacesta.sql"
+
 ECHO =========================
 rar a C:\xampp\htdocs\lepsiacesta\db_lepsiacesta.rar C:\xampp\htdocs\lepsiacesta\db_lepsiacesta.sql
 ECHO ==========================
+
 del C:\xampp\htdocs\lepsiacesta\db_lepsiacesta.sql
+
+REM Git add všetkých zmien
+git add -A
+
+REM Commit s parametrom (ak nie je zadaný, použije default message)
 set arg1=%1
+if "%arg1%"=="" set arg1=Automated commit
 git commit -m "%arg1%"
+
+REM Push na remote
 git push origin master
